e86264e59ee5593044f7d3ec58b012ae83bebc52
Alex Yixuan Xu <yx1137@nyu.edu>
2019-03-12 16:36:45 -0400

    added project 3

--- project3/README.md
+++ project3/README.md
@@ -1,2 +1,8 @@
 # Project 3
 This folder contains only the necessary code for viewing purposes as the original files are too large.
+
+This is a two-part project.
+
+For the 1st one, press key 1 or 2 or 3 then press space to display. Press enter to save images.
+
+Then run the 2nd program, which will randomly draw out the images you saved at first.
---
+++ project3/part2/src/particle.hpp
@@ -0,0 +1,40 @@
+//
+//  particle.hpp
+//  hw_03_drawing
+//
+//  Created by Jerry-Wanglaws on 3/10/19.
+//
+
+#ifndef particle_hpp
+#define particle_hpp
+
+
+#include "ofMain.h"
+#include <vector>
+//#include <stdio.h>
+
+class Particles{
+    
+public:
+    Particles();
+    void setup(ofImage img, int scl, int col);
+    void update();
+    void draw();
+    void follow(vector<ofVec2f> flowField);
+    void applyForce(ofVec2f force);
+    void updatePrev();
+    void edges();
+    
+    int size = 1;
+    int scl;
+    int col;
+    
+    ofImage img;
+    ofVec2f prePos;
+    ofVec2f pos;
+    ofVec2f vel;
+    ofVec2f acc;
+    
+};
+
+#endif /* particle_hpp */
